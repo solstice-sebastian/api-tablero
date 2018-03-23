31f80ec3f0160777938ff4d7fcd039d3e4defed5
aricallen <aric.allen2@gmail.com>
2018-03-22 19:13:53 -0700

    initial ws connection

--- index.js
+++ index.js
@@ -4,4 +4,25 @@
  * poller.js to run trail-bull
  * log results
  * email results
+ *
+ * wss://stream.binance.com:9443/ws/symbol@streamName
+ * <symbol>@kline_<interval>
  */
+require('dotenv').config();
+// const fetch = require('node-fetch');
+// const Ticker = require('./models/ticker.js');
+// const Constants = require('./common/constants.js');
+const WebSocket = require('ws');
+
+// subscribe to ticker prices
+// const interval = Constants.binance.intervals.ONE_MINUTE;
+// const channelName = `neobtc@kline_${interval}`;
+// const address = `wss://stream.binance.com:9443/ws/${channelName}`;
+const address = 'wss://stream.binance.com:9443/ws/bnbbtc@depth';
+const ws = new WebSocket(address);
+
+ws.on('message', (data) => {
+  console.log('data', JSON.stringify(data));
+});
+
+ws.on('error', (err) => console.log('errored', err));
--- modules/exchange-info.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-const { namespace } = require('../private/config.js')();
-
-const getExchangeInfo = () =>
-  fetch(`${namespace}/api/v1/exchangeInfo`)
-    .then((response) => response.json())
-    .then((data) => data);
-
-module.exports = getExchangeInfo;

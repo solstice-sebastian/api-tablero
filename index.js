a5675857421295ebaf55525087efb29e8b6a0441
aricallen <aric.allen2@gmail.com>
2018-03-27 09:32:44 -0700

    emailer setup

--- index.js
+++ index.js
@@ -8,17 +8,25 @@
  * <symbol>@kline_<interval>
  */
 require('dotenv').config();
+const WebSocket = require('ws');
+
 const BinanceCandle = require('./models/binance/candle.js');
 const TrailBull = require('./modules/trail-bull.js');
 const Constants = require('./common/constants.js');
-const WebSocket = require('ws');
+const Emailer = require('./modules/emailer.js');
+const { getPercentDiff } = require('./common/helpers.js')();
 
 // subscribe to ticker prices
 const interval = Constants.binance.intervals.ONE_MINUTE;
 const channelName = `neobtc@kline_${interval}`;
 const address = `wss://stream.binance.com:9443/ws/${channelName}`;
-// const address = 'wss://stream.binance.com:9443/ws/bnbbtc@depth';
 const ws = new WebSocket(address);
+
+// setup emailer
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const emailer = Emailer({ username, password, host });
 
 ws.on('error', (err) => console.log('errored', err));
 
@@ -29,13 +37,30 @@ const buffer = -0.01;
 let startTicker = null;
 let trailBull;
 
+/**
+ * @param {TradeResult} result
+ */
+const exitTrade = (result) => {
+  // email results
+  const profitLoss = getPercentDiff(startTicker.price, result.price);
+  emailer.send({
+    to: 'solstice.sebastian@gmail.com',
+    text: `
+      trade exited with profit loss of ${profitLoss.toFixed(3) * 100}%
+      startPrice: ${startTicker.price}
+      endPrice: ${result.price}
+    `,
+  });
+};
+
 ws.on('message', (response) => {
   const candle = new BinanceCandle(JSON.parse(response));
   if (startTicker === null) {
     startTicker = candle;
     trailBull = TrailBull({ startTicker, buffer });
   } else {
-    const result = new TradeResult(trailBull.update({ ticker }));
+    const ticker = candle;
+    const result = trailBull.update({ ticker });
     if (result.shouldContinue === false) {
       // trade exited
       exitTrade(result);

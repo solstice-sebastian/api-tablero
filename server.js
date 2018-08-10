097b035675b0d7d54ccc05d5be5c1f685cc35c14
aricallen <aric.allen2@gmail.com>
2018-08-10 09:47:10 -0700

    refactor out trade-record-manager

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const https = require('https');
 const BinanceDashboard = require('./binance/dashboard.js');
 const GdaxDashboard = require('./gdax/dashboard.js');
 const CoinigyTickerBook = require('./coinigy/ticker-book.js');
-const TradeRecordManager = require('./modules/trade-record-manager.js');
 const CoinigyAdapter = require('./coinigy/adapter.js');
 const MockTickerBook = require('./mocks/ticker-book.js');
 const commandLineArgs = require('command-line-args');
@@ -16,12 +15,10 @@ const serializeDashboards = require('./serializers/dashboard.js');
 const cmdDefs = [{ name: 'mock', alias: 'm', type: Boolean }];
 const cmdLineOptions = commandLineArgs(cmdDefs);
 
-// const { requestMethods } = Constants;
 const PORT = process.env.PORT || 5000;
-const { ALLOW_ORIGIN, RECORD_MANAGER_USER, RECORD_MANAGER_PWD, ENVIRONMENT } = process.env;
+const { ALLOW_ORIGIN, ENVIRONMENT } = process.env;
 
 const tickerBook = cmdLineOptions.mock ? new MockTickerBook() : new CoinigyTickerBook();
-const tradeRecordManager = new TradeRecordManager();
 
 const app = express();
 app.use(bodyParser.json());
@@ -34,25 +31,6 @@ const allowCrossDomain = (req, res, next) => {
   next();
 };
 app.use(allowCrossDomain);
-const simpleAuth = (req, res, next) => {
-  if (req.headers.origin !== ALLOW_ORIGIN) {
-    try {
-      const { username, password } = JSON.parse(req.headers.authorization);
-      if (username === RECORD_MANAGER_USER && password === RECORD_MANAGER_PWD) {
-        return next();
-      }
-      throw new Error('Record manager authorization failed');
-    } catch (e) {
-      if (req.headers.authorization === undefined) {
-        throw new Error('credentials missing', e);
-      }
-      throw new Error('Record manager authorization failed', e);
-    }
-  } else {
-    return next();
-  }
-};
-// app.use(simpleAuth);
 
 app.get('/dashboards', async (req, res) => {
   const dashboards = [];
@@ -89,35 +67,6 @@ app.get('/tickers', async (req, res) => {
   res.send(latest.serialize());
 });
 
-app.post('/trade-record', async (req, res) => {
-  const { ticker, strategy, trader } = req.body;
-  const result = await tradeRecordManager.process({ ticker, strategy, trader });
-  if (result instanceof Error) {
-    return res.status(400).send(result);
-  }
-  return res.send(result);
-});
-
-app.put('/trade-record', async (req, res) => {
-  const { ticker, strategy } = req.body;
-  const result = await tradeRecordManager.update({ ticker, strategy });
-  if (result instanceof Error) {
-    return res.status(400).send(result);
-  }
-  return res.send(result);
-});
-
-app.get('/trade-record', async (req, res) => {
-  const result = await tradeRecordManager.find({});
-  return res.send(result);
-});
-
-app.delete('/trade-record', async (req, res) => {
-  const { ticker } = req.body;
-  const result = await tradeRecordManager.remove({ ticker });
-  return res.send(result);
-});
-
 if (ENVIRONMENT !== Constants.environments.PRODUCTION) {
   const credentials = {
     key: fs.readFileSync('certs/server.key', 'utf8'),
@@ -129,7 +78,7 @@ if (ENVIRONMENT !== Constants.environments.PRODUCTION) {
   app.listen(PORT);
 }
 
-const request = require('request');
+// const request = require('request');
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-request.get(`https://localhost:${PORT}/dashboards`);
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+// request.get(`https://localhost:${PORT}/dashboards`);
